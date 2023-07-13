1e01682d01b72a6cf61e85f20d96287275767342
Anton20241 <ilyasov.toni@mail.ru>
2023-07-13 16:24:30 +0300

    stable

--- src/fingers/build/catkin_generated/setup_cached.sh
+++ src/fingers/build/catkin_generated/setup_cached.sh
@@ -7,10 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel:/home/anton20241/pipe_inspect_ws/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/lib:/home/anton20241/pipe_inspect_ws/devel/lib:/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/lib/pkgconfig:/home/anton20241/pipe_inspect_ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build'
-export PYTHONPATH="/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/lib/python3/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers:$ROS_PACKAGE_PATH"
+export PYTHONPATH='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/lib/python3/dist-packages:/home/anton20241/pipe_inspect_ws/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers/build/devel/share/common-lisp:/home/anton20241/pipe_inspect_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/anton20241/rtc_work/fingers_rx_tx_ws/src/fingers:/home/anton20241/pipe_inspect_ws/src:/opt/ros/noetic/share'
